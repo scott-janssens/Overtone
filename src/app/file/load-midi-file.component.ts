5b64c33ae76b44f37c6874b04449a806b99f46cd
Scott Janssens <scottjanssensdev@gmail.com>
2024-02-10 13:53:08 -0600

    File reload now possible

--- src/app/file/load-midi-file.component.ts
+++ src/app/file/load-midi-file.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { MidiService } from "../services/midi/midi.service";
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import { MatIcon} from '@angular/material/icon';
@@ -10,11 +10,17 @@ import { MatIcon} from '@angular/material/icon';
     imports: [MatProgressSpinner, MatIcon],
     standalone: true
 })
-export class LoadMidiFileComponent {
+export class LoadMidiFileComponent implements OnInit {
+    private _fileInput: HTMLInputElement | undefined;
+
     private _filename: string | null = null;
     get filename(): string | null { return this._filename; }
 
     constructor(private _midiService: MidiService) {
+    }
+
+    ngOnInit(): void {
+        this._fileInput = document.getElementById("file-input") as HTMLInputElement;
     }
 
     public async onLoad(event: any) {
@@ -24,5 +30,7 @@ export class LoadMidiFileComponent {
             this._filename = file.name;
             await this._midiService.loadMidiFileAsync(file);
         }
+
+        this._fileInput!.value = "";
     }
 }

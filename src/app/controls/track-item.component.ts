f29701f9e9e7d071d5c07e53d6ba04d249c25847
Scott Janssens <scottjanssensdev@gmail.com>
2024-02-13 01:12:14 -0600

    Color picker changed and styled

--- src/app/controls/track-item.component.ts
+++ src/app/controls/track-item.component.ts
@@ -1,8 +1,7 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatCheckbox } from '@angular/material/checkbox';
-import { ColorPickerModule } from 'ngx-color-picker';
 import { MatSelect, MatOption } from '@angular/material/select';
 import { MidiTrack } from '../services/midi/MidiTrack';
 import { ProgramChange, ProgramChanges } from '../services/midi/ProgramChanges';
@@ -10,13 +9,15 @@ import { ProgramChange, ProgramChanges } from '../services/midi/ProgramChanges';
 @Component({
   selector: 'ot-track-item',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatCheckbox, MatSelect, MatOption, ColorPickerModule],
+  imports: [CommonModule, FormsModule, MatCheckbox, MatSelect, MatOption], //, ColorPickerModule],
   templateUrl: './track-item.component.html',
   styleUrl: './track-item.component.scss'
 })
 export class TrackItemComponent {
   @Input() track: MidiTrack | undefined;
   @Output() notify: EventEmitter<MidiTrack> = new EventEmitter<MidiTrack>();
+  @ViewChild("colorPickerContainer", { static: false }) colorPickerContainer!: HTMLDivElement;
+  @ViewChild("colorPicker", { read: ElementRef, static: false }) colorPicker!: HTMLInputElement;
 
   get programChanges(): ProgramChange[] {
     return ProgramChanges.programChanges.sort((a, b) => (a.instrument > b.instrument ? 1 : -1));
